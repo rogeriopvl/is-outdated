77f27c16a0ff1ded104fbfefb1ac88d06870b6a0
rogeriopvl <rogeriopvl@gmail.com>
2015-07-21 12:20:48 +0100

    Mock npm module when testing

--- test/index.js
+++ test/index.js
@@ -1,24 +1,43 @@
 var test = require('tape');
+var mockery = require('mockery');
 var isOutdated = require('../');
 
-test('annoyer exists as a function', function (t) {
-  t.plan(1);
+// Mock the npm module
+var npmMock = {
+  load: function (opts, cb) {
+    return cb();
+  },
+  commands: {
+    info: function (args, silent, cb) {
+      return cb(null, { '2.0.0': { version: '2.0.0' }});
+    }
+  }
+};
+mockery.registerMock('npm', npmMock);
+
+test('is-outdated exists as a function', function (t) {
   t.ok(typeof isOutdated === 'function');
+  t.end();
 });
 
 test('given a package name and inferior version should return and object with version', function (t) {
+  mockery.enable();
   t.plan(3);
-  isOutdated('windrose', '1.0.0', function (err, data) {
+  isOutdated('mypackage', '1.0.0', function (err, data) {
     t.error(err);
     t.ok(data);
     t.equal(Object.keys(data)[0], 'version');
   });
+  t.on('end', mockery.disable);
 });
 
 test('given a package name and equal or superior version should return false', function (t) {
+  mockery.enable();
+
   t.plan(2);
-  isOutdated('windrose', '3.0.0', function (err, data) {
+  isOutdated('mypackage', '3.0.0', function (err, data) {
     t.error(err);
     t.notOk(data);
   });
+  t.on('end', mockery.disable);
 });
